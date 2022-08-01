99480e385d527a5d80945483d979b5dbe9fbf2ff
pjy <3454859304@qq.com>
2022-08-01 11:47:46 +0800

    再次开始刷题

--- 0016. 最接近的三数之和/src/Solution.java
+++ 0016. 最接近的三数之和/src/Solution.java
@@ -1,9 +1,43 @@
+import java.util.Arrays;
+
 /**
  * 功能描述
  *
  * @author: PJY
  * @date: 2022年07月31日 15:24
  */
-public class Solution {
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        boolean flag = false;
+        for(int i=0;i<nums.length-2;i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            if (i > 0 && 3*nums[i] -target > diff ) break;
+            for(int left=i+1,right=nums.length-1;left<right;){
+                int temp =nums[i]+nums[left]+nums[right] -target;
+                if(temp==0){
+                    return target;
+                }else if(temp<0){
+                    left++;
+                    temp=-temp;
+                    if(temp<diff){
+                        diff=temp;
+                        flag=false;
+                    }
+                }else {
+                    right--;
+                    if(temp<diff){
+                        diff=temp;
+                        flag=true;
+                    }
+                }
+            }
+        }
+        if(flag){
+            return target+diff;
+        }
+        return target-diff;
+    }
 
 }
---
+++ 0017. 电话号码的字母组合/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+/**
+ * 功能描述
+ *
+ * @author: PJY
+ * @date: 2022年08月01日 11:47
+ */
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+}
