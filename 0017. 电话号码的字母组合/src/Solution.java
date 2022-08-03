5d450471b4305e2b1f63c15baa06d192ec43aec0
pjy <3454859304@qq.com>
2022-08-03 12:54:53 +0800

    2022.8.3

--- 0017. 电话号码的字母组合/src/Solution.java
+++ 0017. 电话号码的字母组合/src/Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,55 @@ import java.util.List;
  */
 public class Solution {
     public List<String> letterCombinations(String digits) {
-        return null;
+        if(digits.length()==0){
+            return new ArrayList<>();
+        }
+
+        char[][] chars = new char[8][];
+        chars[0]= new char[]{'a','b','c'};
+        chars[1]= new char[]{'d','e','f'};
+        chars[2]= new char[]{'g','h','i'};
+        chars[3]= new char[]{'j','k','l'};
+        chars[4]= new char[]{'m','n','o'};
+        chars[5]= new char[]{'p','q','r','s'};
+        chars[6]= new char[]{'t','u','v'};
+        chars[7]= new char[]{'w','x','y','z'};
+
+        ArrayList<String> strings = new ArrayList<>();
+        char[] cd=digits.toCharArray();
+        getStrings(strings,chars,0,cd,new StringBuilder());
+        return strings;
+
+    }
+
+    public void getStrings(List<String> strings,char[][] charss,int  index, char[] cd,StringBuilder str){
+        if(index==cd.length){
+            strings.add(str.toString());
+        }else{
+            char[] chars=charss[cd[index]-'2'];
+            for(int i=0;i<chars.length;i++){
+                str.append(chars[i]);
+                getStrings(strings,charss,index+1,cd,str);
+                str.deleteCharAt(index);
+            }
+        }
+    }
+
+    public char[] getCharList(char c){
+        switch (c){
+            case '2': return new char[]{'a','b','c'};
+            case '3': return new char[]{'d','e','f'};
+            case '4': return new char[]{'g','h','i'};
+            case '5': return new char[]{'j','k','l'};
+            case '6': return new char[]{'m','n','o'};
+            case '7': return new char[]{'p','q','r','s'};
+            case '8': return new char[]{'t','u','v'};
+            case '9': return new char[]{'w','x','y','z'};
+            default: return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Solution().letterCombinations("23");
     }
 }
