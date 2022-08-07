10583b21f47b31afa02def76e2d657df6a8a6382
pjy <3454859304@qq.com>
2022-08-07 18:43:44 +0800

    2022.8.7 哭了都不会

--- 0029. 两数相除/src/Solution.java
+++ 0029. 两数相除/src/Solution.java
@@ -5,7 +5,8 @@
  * @date: 2022年08月06日 17:57
  */
 public class Solution {
-    public static void main(String[] args) {
-
+    public int divide(int dividend, int divisor) {
+        //TODO 两数相除
+        return 1^1;
     }
 }
---
+++ 0050. 快速幂Pow(x, n)/src/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * 功能描述
+ *
+ * @author: PJY
+ * @date: 2022年08月07日 16:14
+ */
+public class Solution {
+    public double myPow(double x, int n) {
+        //TODO 快速幂算法
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
