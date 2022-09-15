a5e18f214baff2cffb48f701a5609942c245884d
pjy <3454859304@qq.com>
2022-09-15 17:19:00 +0800

    2022.9.15

--- 0042. 接雨水/src/Solution.java
+++ 0042. 接雨水/src/Solution.java
@@ -5,8 +5,44 @@
  * @date: 2022年09月02日 15:23
  */
 public class Solution {
+
     public int trap(int[] height) {
-        //TODO hard
-        return 0;
+        int ans = 0;
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+
+    public int trap0(int[] height) {
+        int sum = 0;
+        int max_left = 0, max_right = 0;
+        int left = 1, right = height.length - 2;
+        while (left <= right) {
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                if (max_left > height[left]) {
+                    sum = sum + (max_left - height[left]);
+                }
+                left++;
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                if (max_right > height[right]) {
+                    sum = sum + (max_right - height[right]);
+                }
+                right--;
+            }
+        }
+        return sum;
     }
 }
