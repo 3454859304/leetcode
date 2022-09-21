b4670566fdfa9c582a841a03a9f8cb05f7a096bf
pjy <3454859304@qq.com>
2022-09-21 22:13:55 +0800

    2022.9.21

--- 0046. 全排列/src/Solution.java
+++ 0046. 全排列/src/Solution.java
@@ -1,4 +1,10 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 功能描述
@@ -9,6 +15,27 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         //TODO medium
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        dfs(result,0,nums);
+        return result;
+    }
 
+    private void dfs(ArrayList result, int idx,int[] nums) {
+        if(idx==nums.length){
+            ArrayList<Object> list = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                list.add(nums[i]);
+            }
+            result.add(list);
+        }else{
+            for(int i=idx;i<nums.length;i++){
+                int temp = nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=temp;
+                dfs(result,idx+1,nums);
+                nums[idx]=nums[i];
+                nums[i]=temp;
+            }
+        }
     }
 }
---
+++ 0047. 全排列 II/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * 功能描述
+ *
+ * @author: PJY
+ * @date: 2022年09月21日 22:12
+ */
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        //TODO medium
+        return null;
+    }
+}
