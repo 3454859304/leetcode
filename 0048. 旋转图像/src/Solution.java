406823a5408d906d4c42e45a8fa2fcf6dd3eeaaf
pjy <3454859304@qq.com>
2022-09-23 16:42:19 +0800

    2022.9.23

--- 0048. 旋转图像/src/Solution.java
+++ 0048. 旋转图像/src/Solution.java
@@ -7,5 +7,17 @@
 public class Solution {
     public void rotate(int[][] matrix) {
         //TODO medium
+        int n= matrix.length;
+        int temp ;
+        for(int i= 0; i<n/2;i++){
+            for(int j =0;j<n/2+(n&1);j++){
+                temp=matrix[n-1-j][i];
+                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+                matrix[j][n-1-i]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
     }
+
 }
---
+++ 0049. 字母异位词分组/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * 功能描述
+ *
+ * @author: PJY
+ * @date: 2022年09月23日 16:30
+ */
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //TODO medium
+
+    }
+}
