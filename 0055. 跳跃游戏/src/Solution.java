5892f7e723f29d09bf566b15efcabcb5c80c8b7b
pjy <3454859304@qq.com>
2022-10-28 14:51:56 +0800

    2022.10.28 test git

--- 0055. 跳跃游戏/src/Solution.java
+++ 0055. 跳跃游戏/src/Solution.java
@@ -5,8 +5,13 @@
  * @date: 2022年10月04日 21:49
  */
 public class Solution {
+    //sb
     public boolean canJump(int[] nums) {
-        //TODO medium
-        return false;
+        int end =0;
+        for(int i=0;i<nums.length;i++){
+            if(i>end) return false;
+            else if(nums[i]>end-i) end = nums[i]+i;
+        }
+        return true;
     }
 }
